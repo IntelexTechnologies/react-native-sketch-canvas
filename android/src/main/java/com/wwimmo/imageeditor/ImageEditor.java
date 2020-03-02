04474236a424559bd53be0d86310403cc69cfc1f
bobowinca <bobowinca@gmail.com>
2020-03-02 10:06:08 -0500

    fix tiny text on first add

--- android/src/main/java/com/wwimmo/imageeditor/ImageEditor.java
+++ android/src/main/java/com/wwimmo/imageeditor/ImageEditor.java
@@ -750,7 +750,7 @@ public class ImageEditor extends View {
         }
 
         TextEntity textEntity = null;
-        if (mSketchCanvas.getWidth() < 120 || mSketchCanvas.getHeight() < 120) {
+        if (mSketchCanvas.getWidth() < 130 || mSketchCanvas.getHeight() < 130) {
             textEntity = new TextEntity(textLayer, mDrawingCanvas.getWidth(), mDrawingCanvas.getHeight());
         } else {
             textEntity = new TextEntity(textLayer, mSketchCanvas.getWidth(), mSketchCanvas.getHeight());
